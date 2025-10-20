01396cd44d925aaec33e4b4b3e71def163fdd153
becherryu <81617058+becherryu@users.noreply.github.com>
2025-10-20 18:36:44 +0900

    feat(App.js): 기본 계산기 구현
    - 사용자 입력(빈 문자열 처리)
    - 기본 구분자(쉼표, 콜론) 기준 합산

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await getInput();
+
+      if (input.trim() === "") {
+        printResult(0);
+        return;
+      }
+
+      const numbers = getNumbers(input);
+      const sum = calculateSum(numbers);
+
+      printResult(sum);
+    } catch (error) {
+      Console.print(`[ERROR]`);
+      throw error;
+    }
+  }
 }
 
+// 1. 사용자 입력
+async function getInput() {
+  return Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+}
+
+function getNumbers(input) {
+  const SEPARATOR = /,|:/;
+
+  return input.split(SEPARATOR).map(Number);
+}
+
+function calculateSum(numbers) {
+  return numbers.reduce((sum, num) => sum + num, 0);
+}
+
+function printResult(sum) {
+  Console.print(`결과 : ${sum}`);
+}
 export default App;
